b56a3beebab34b3b1955637cfb49e8c518cb75b0
shahzebmqs <48654305+shahzebmqs@users.noreply.github.com>
2020-04-27 12:20:45 +0200

    Add files via upload

---
+++ Tp3_Shahzeb_MAQSOOD/www/js/exo2.js
@@ -0,0 +1,6 @@
+document.write("Allo la Terre !! <br>");
+prompt("Votre nom?","indiquez votre nom ici");
+alert("Quel est votre prénom?");
+confirm("Choisir le bouton OK ou annuler");
+console.log("Je m'affiche depuis la console");
+document.write("AU REVOIR");
---
+++ Tp3_Shahzeb_MAQSOOD/www/js/exo3.js
@@ -0,0 +1,4 @@
+var neuf=9;
+var mult_neuf = prompt("Quel nombre voulez-vous multiplier à 9 ?");
+var mult = neuf*mult_neuf;
+document.write(mult);
